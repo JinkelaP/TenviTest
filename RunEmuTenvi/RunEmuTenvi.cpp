ab518f397c5301175b2487fcc974df3d481a79a0
Riremito <29014872+Riremito@users.noreply.github.com>
2023-11-08 23:26:26 +0900

    injector fix

--- RunEmuTenvi/RunEmuTenvi.cpp
+++ RunEmuTenvi/RunEmuTenvi.cpp
@@ -32,6 +32,28 @@ bool RunEmu(std::wstring wDroppedTarget) {
 	return injector.Run(wCmdLine);
 }
 
+
+bool RunEmuTest(std::wstring wDroppedTarget, std::wstring wRegion) {
+	Config conf(EXE_NAME".ini");
+	std::wstring wTarget, wCmdLine, wLoaderDLL, wMainDLL;
+	bool check = true;
+	conf.Read(EXE_NAME, L"CmdLine", wCmdLine);
+	check &= conf.Read(EXE_NAME, L"LoaderDLL", wLoaderDLL);
+	conf.Update(EXE_NAME, L"Region", wRegion);
+
+	if (!check) {
+		return false;
+	}
+
+	std::wstring dir;
+	if (!GetDir(dir)) {
+		return false;
+	}
+
+	Injector injector(wDroppedTarget, dir + wLoaderDLL);
+	return injector.Run(wCmdLine);
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
 
 	if (__argc < 2) {
@@ -40,6 +62,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 		}
 	}
 	else {
+		if (__argc == 3) {
+			if (!RunEmuTest(__wargv[1], __wargv[2])) {
+				MessageBoxW(NULL, L"error: cmdline = path region", EXE_NAME, MB_OK);
+			}
+			return 0;
+		}
 		if (!RunEmu(__wargv[1])) {
 			MessageBoxW(NULL, L"Please drop target exe file", EXE_NAME, MB_OK);
 		}
